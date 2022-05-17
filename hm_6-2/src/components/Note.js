62d394beccaa842457551c8102382235ebd478f3
Denis Nikitin <dinya.nikitin@gmail.com>
2022-05-17 22:09:08 +0300

    finish

---
+++ hm_6-2/src/components/Note.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Note(props) {
+    const {item} = props;
+    const {func} = props;    
+  return (<div className='note' >
+    <span className='noteSpan' >{item.content}</span>
+    <button className='noteSpanButton' id={item.id} onClick={func}>&#10060;</button>
+    </div>
+  )
+}
+
+Note.propTypes = {
+    item: PropTypes.object.isRequired,
+}
+
+export default Note
