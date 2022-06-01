fdd8bf381255ddcdf0ec454502ad8b1a5a1e2c33
Denis Nikitin <dinya.nikitin@gmail.com>
2022-06-01 14:45:40 +0300

    fix

--- hm_6-2/src/App.js
+++ hm_6-2/src/App.js
@@ -1,87 +1,90 @@
 import { nanoid } from 'nanoid';
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import Note from './components/Note';
 
 function App() {
- const [note, setNote] = useState('');
- const [notesAll, setNotesAll] = useState([]);
+  const [note, setNote] = useState('');
+  const [notesAll, setNotesAll] = useState([]);
 
- const loadNotes = () => {
-   fetch('http://localhost:7777/notes')
-      .then(response => response.json()) 
-      .then(notes => {console.log(notes)
+  const loadNotes = () => {
+    fetch('http://localhost:7777/notes')
+      .then(response => response.json())
+      .then(notes => {
         setNotesAll(prev => prev = notes);
-      })  
- }
- 
- const addNote = (item) => {
-   fetch('http://localhost:7777/notes', {
-    method: 'POST',
-    body: JSON.stringify(item),
-  headers: {
-    'Content-Type': 'application/json'}
-   })
-   .then(response => 
-    {if(response.status === 204)
-    {loadNotes()}}
-    )
- }
-
- const onSubmitHandle = (e) => {
-  e.preventDefault();
-  const item = {
-    id: nanoid(),
-    content: note,
+      })
   }
-  
-  addNote(item)
+
+  const addNote = (item) => {
+    fetch('http://localhost:7777/notes', {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.status === 204) { loadNotes() }
+      }
+      )
+  }
+
+  const onSubmitHandle = (e) => {
+    e.preventDefault();
+    const item = {
+      id: nanoid(),
+      content: note,
+    }
+
+    addNote(item)
     setNote('')
- }
+  }
 
- const onRemove = (e) => {
-   console.log(e.target)
-   const id = e.target.id;
-   fetch(`http://localhost:7777/notes/${id}`,{
-     method: 'DELETE'
-   })
-   .then(response => {if(response.status === 204){
-     loadNotes();
-   }})
- }
- const onChangeHandle = (e) => {
-  setNote(prev => prev = e.target.value);
- }
+  const onRemove = (e) => {
+    console.log(e.target)
+    const id = e.target.id;
+    fetch(`http://localhost:7777/notes/${id}`, {
+      method: 'DELETE'
+    })
+      .then(response => {
+        if (response.status === 204) {
+          loadNotes();
+        }
+      })
+  }
+  const onChangeHandle = (e) => {
+    setNote(prev => prev = e.target.value);
+  }
 
- const onClickUpload = () => {
-   loadNotes();
- }
- useEffect(() => {
-   loadNotes()
- }, [])
+  const onClickUpload = () => {
+    loadNotes();
+  }
+  useEffect(() => {
+    loadNotes()
+  }, [])
 
-useEffect(() => {
-   console.log(notesAll)
-}, [notesAll])
+  useEffect(() => {
+    console.log(notesAll)
+  }, [notesAll])
 
 
 
-   return (<div className='notesBox'>
-   <div className='notesBoxTitle'>
+  return (<div className='notesBox'>
+    <div className='notesBoxTitle'>
       <h2 className='noteTitle'>Notes</h2>
       <button className='notesButtonUpload' onClick={onClickUpload}>&#128472;</button>
-   </div>
-   <div className='noteBoxNotes'>
-   {notesAll.map(el => <Note item={el}  key={el.id} func={onRemove}/>)}
-   </div> 
+    </div>
+    <div className='noteBoxNotes'>
+      {notesAll.map(el => <Note item={el} key={el.id} func={onRemove} />)}
+    </div>
     <form className='noteForm' onSubmit={onSubmitHandle}>
-     <label for='noteInput' className='noteLabel' >New note
-     <input className='noteInput' name='noteInput' type='text' required onChange={onChangeHandle} value={note}/></label>
-     <button className='noteButton'>&#10148;</button>
+      <label for='noteInput' className='noteLabel' >New note
+        <input className='noteInput' name='noteInput' type='text' required onChange={onChangeHandle} value={note} /></label>
+      <button className='noteButton'>&#10148;</button>
     </form>
   </div>
-    
-   )
+
+  )
 }
 
 
--- hm_6-2/src/components/Note.js
+++ hm_6-2/src/components/Note.js
@@ -2,17 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function Note(props) {
-    const {item} = props;
-    const {func} = props;    
+  const { item } = props;
+  const { func } = props;
   return (<div className='note' >
     <span className='noteSpan' >{item.content}</span>
     <button className='noteSpanButton' id={item.id} onClick={func}>&#10060;</button>
-    </div>
+  </div>
   )
 }
 
 Note.propTypes = {
-    item: PropTypes.object.isRequired,
+  item: PropTypes.object.isRequired,
 }
 
 export default Note
